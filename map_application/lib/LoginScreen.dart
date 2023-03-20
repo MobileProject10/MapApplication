c8e30a990842725c692dbdf46b0a0784ba0d3c5c
Jussi <t1leju02@students.oamk.fi>
2023-03-20 12:13:22 +0200

    Kirjautumislomakkeen tyylittelyä

--- map_application/lib/LoginScreen.dart
+++ map_application/lib/LoginScreen.dart
@@ -5,8 +5,25 @@ import 'package:google_maps_in_flutter/main.dart';
 class LoginScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _LoginScreenState();
-  
 }
+
+//VIP
+Widget buildHeader(){
+  return Column(
+    children: <Widget>[
+      SizedBox(
+        height: 60,
+        width: 500,
+        child: const DecoratedBox(
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 13, 141, 192),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+//
 
 Widget buildUsername() {
   return Column(
@@ -14,6 +31,7 @@ Widget buildUsername() {
     children: <Widget>[
       SizedBox(height: 10),
       Container(
+        margin: EdgeInsets.only(left: 30, right: 30),
         alignment: Alignment.centerLeft,
         decoration: BoxDecoration(
           color: Colors.white,
@@ -26,7 +44,7 @@ Widget buildUsername() {
             )
           ]
         ),
-        height: 60,
+        height: 45,
         child: TextField(
           keyboardType: TextInputType.name,
           style: TextStyle(
@@ -52,6 +70,7 @@ Widget buildPassword() {
     children: <Widget>[
       SizedBox(height: 10),
       Container(
+        margin: EdgeInsets.only(left: 30, right: 30),
         alignment: Alignment.centerLeft,
         decoration: BoxDecoration(
           color: Colors.white,
@@ -64,7 +83,7 @@ Widget buildPassword() {
             )
           ]
         ),
-        height: 60,
+        height: 45,
         child: TextField(
           obscureText: true,
           style: TextStyle(
@@ -86,27 +105,39 @@ Widget buildPassword() {
 
 Widget buildLoginBtn(){
   return Container(
+    margin: EdgeInsets.only(left: 100, right: 100),
     padding: EdgeInsets.symmetric(vertical: 25),
     width: double.infinity,
     child: ElevatedButton(
       onPressed: () => {
-        runApp(MyApp()
-        )
+        runApp(MyApp())
       },
       style: ElevatedButton.styleFrom(
         padding: EdgeInsets.all(15),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20)
+        )
       ),
       child: Text(
-        'Kirjaudu'
+        'Kirjaudu',
+         style: TextStyle(
+          fontSize: 17,
+          fontWeight: FontWeight.normal,
+         ),
+
       ),
     ),
-  );
+    );
 }
 
 class _LoginScreenState extends State<LoginScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          title: const Text('Google Maps In Flutter'),
+          elevation: 2,
+        ),
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.light,
         child: GestureDetector(
@@ -116,29 +147,11 @@ class _LoginScreenState extends State<LoginScreen>{
                 height: double.infinity,
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color(0xff),
-                      Color(0xff),
-                      Color(0xff),
-                      Color(0xff),
-                    ]
-                  )
+                  color: Color.fromARGB(255, 228, 228, 228),
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text(
-                      'Google Maps In Flutter',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 24, 89, 187),
-                        fontSize: 40,
-                        fontWeight: FontWeight.normal
-                      ),
-                    ),
-                    SizedBox(height: 20),
                     buildUsername(),
                     buildPassword(),
                     buildLoginBtn(),
