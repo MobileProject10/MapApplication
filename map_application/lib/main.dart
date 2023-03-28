90166e6222d62e54b346e73098f850fdec5194b5
Juho Soivio <soivio.juho@gmail.com>
2023-03-28 16:01:23 +0300

    Burgermenu ja asetukset-sivu

--- map_application/lib/main.dart
+++ map_application/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'widgets/settings.dart';
 
-void main() => runApp(const MyApp());
+
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -9,6 +12,8 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
+
 
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
@@ -31,6 +36,44 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Maps Sample App'),
           elevation: 2,
         ),
+        drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Asetukset'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                runApp(Settings());
+                //Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Kirjaudu ulos'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
